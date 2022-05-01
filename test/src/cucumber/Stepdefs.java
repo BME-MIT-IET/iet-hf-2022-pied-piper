da8cbaf68fd6e927b7dc6c267f9cafc6c99df0b3
barnabassoros <soros14@gmail.com>
2022-05-01 14:53:52 +0200

    Dummy cucumber test created

---
+++ test/src/cucumber/Stepdefs.java
@@ -0,0 +1,32 @@
+package cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.*;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
+
+public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
