2031029f0ecfb716c3f1ebe21262656f8a907ddd
Máté Meglécz <meglecz.mate@gmail.com>
2022-05-15 18:09:28 +0200

    full load test

--- test/src/com/complexible/pinto/LoadTest.java
+++ test/src/com/complexible/pinto/LoadTest.java
@@ -2,17 +2,37 @@ package com.complexible.pinto;
 
 import org.jsmart.zerocode.core.domain.LoadWith;
 import org.jsmart.zerocode.core.domain.TestMapping;
+import org.jsmart.zerocode.core.domain.TestMappings;
 import org.jsmart.zerocode.core.runner.parallel.ZeroCodeLoadRunner;
+import org.jsmart.zerocode.core.runner.parallel.ZeroCodeMultiLoadRunner;
 import org.junit.runner.RunWith;
 
 //test that will each spawned user execute
-@TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteListOfObjects")
 
-
+@TestMappings({
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testReadRdfListOfObjects"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testReadRdfListOfPrimitives"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWritePrimitives"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testReadPrimitives"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testReadMixed"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteMixed"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteListOfPrimitives"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteListsOfPrimitivesAsRdfListsWithOption"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteListsOfPrimitivesAsRdfListWithAnnotation"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteListOfObjects"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteListsOfObjectsAsRdfListWithOption"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteListsOfObjectsAsRdfListWithAnnotation"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testUseRdfIdForIdentification"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testReadSetsIdentifiableId"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteSetsIdentifiableId"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testWriteWithCodec"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testReadWithCodec"),
+        @TestMapping(testClass = RDFMapperTests.class, testMethod = "testReadMap"),
+})
 //how many users are we going to spawn
 @LoadWith("my_load_config.properties")
 
 //run it with zero code load runner
-@RunWith(ZeroCodeLoadRunner.class)
+@RunWith(ZeroCodeMultiLoadRunner.class)
 public class LoadTest {
 }
